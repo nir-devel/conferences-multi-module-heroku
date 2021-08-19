b430898061c4277abc5eba4ebdd0322a4c060ffa
nir <niritzhak10@gmail.com>
2021-08-19 08:05:58 +0300

    Update the End point :Set class level:"/api/v1/sessions"

--- ConferencesWebParentProject/ConferencesBackEndProject/src/main/java/org/nir/conferences/admin/sessions/SessionRestController.java
+++ ConferencesWebParentProject/ConferencesBackEndProject/src/main/java/org/nir/conferences/admin/sessions/SessionRestController.java
@@ -5,18 +5,22 @@ import java.util.List;
 import org.nir.conferences.common.entity.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/sessions")
 public class SessionRestController
 {
 	@Autowired private SessionService sessionService;
 	
 	
-	@GetMapping("/api/v1/sessions")
+	@GetMapping("/")
 	public List<Session> listSessions()
 	{
 		return this.sessionService.listAll();
 	}
 
+	
+	
 }
